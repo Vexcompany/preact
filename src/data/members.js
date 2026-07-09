508a3e4dbc1a4cfa52c275239a8070a8797fbb68
Vexcompany <vexcorporation43@gmail.com>
2026-07-09 13:22:51 +0700

    Create members.js

---
+++ src/data/members.js
@@ -0,0 +1,128 @@
+const BASE = 'https://iffrqakrixmgobvkzpzc.supabase.co/storage/v1/object/public/foto'
+const LOGO = `${BASE}/logo.png`
+
+// ─── GENERASI 1 ───────────────────────────────────────────────────────────────
+
+export const gen1 = {
+  pembina: [
+    { nama: 'Yulva Romhida', jabatan: 'Pembina', ttl: 'Madiun, 1 Januari 1980', sosmed: '@yulva_alfaiz', foto: LOGO },
+  ],
+  pelatih: [
+    { nama: 'Airlangga Sukmana Teja Bramata', jabatan: 'Pelatih', ttl: 'Madiun, 9 September 1999', sosmed: '@airlanggabrmt_', foto: 'https://img1.pixhost.to/images/11273/677040669_uploaded_image.jpg' },
+    { nama: "Enrico Bryan's", jabatan: 'Pelatih', ttl: 'Madiun, 9 November 2001', sosmed: '@enrico_bryans', foto: LOGO },
+  ],
+  inti: [
+    { nama: 'Ketua Umum Gen1', jabatan: 'Ketua Umum', ttl: 'Madiun, 15 Maret 2007', sosmed: '@gen1', foto: LOGO },
+    { nama: 'Wakil Ketua Umum Gen1', jabatan: 'Wakil Ketua Umum', ttl: 'Madiun, 15 Maret 2007', sosmed: '@gen1', foto: LOGO },
+    { nama: 'Sekretaris Gen1', jabatan: 'Sekretaris', ttl: 'Madiun, 15 Maret 2007', sosmed: '@gen1', foto: LOGO },
+    { nama: 'Bendahara Gen1', jabatan: 'Bendahara', ttl: 'Madiun, 15 Maret 2007', sosmed: '@gen1', foto: LOGO },
+    { nama: 'DTP Gen1 A', jabatan: 'DTP', ttl: 'Madiun, 15 Maret 2007', sosmed: '@gen1', foto: LOGO },
+    { nama: 'DTP Gen1 B', jabatan: 'DTP', ttl: 'Madiun, 15 Maret 2007', sosmed: '@gen1', foto: LOGO },
+  ],
+  divisi: [
+    { nama: 'Rehan Raiszaki Muhfroni', jabatan: 'Koor GK3', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Yasir Isa', jabatan: 'GK3', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'William Cristian Josepa', jabatan: 'GK3', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Dina Tri Handayani', jabatan: 'GK3', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Marsela Evi Novitasari', jabatan: 'GK3', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Naina Wulansari', jabatan: 'Koor Disarda', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Renata Putri Aurellia', jabatan: 'Disarda', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Dwi Yuliana Saputri', jabatan: 'Disarda', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Denis Putri Yuliana', jabatan: 'Disarda', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Ilham Bekti Pratama', jabatan: 'Disarda', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Nataly Reva Ayu Pradifa', jabatan: 'Disarda', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Amanda Amelia M.', jabatan: 'Disarda', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Asiva Rizky Agustin', jabatan: 'Disarda', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Rizky Aditya', jabatan: 'Koor Infokom', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Nisrina Kirana Alya', jabatan: 'Infokom', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Amanda Fidda Reza Azzahara', jabatan: 'Infokom', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Salma Murni Indiannisa', jabatan: 'Infokom', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+    { nama: 'Zitkanisa Ullima', jabatan: 'Infokom', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: LOGO },
+  ],
+}
+
+// ─── GENERASI 2 ───────────────────────────────────────────────────────────────
+
+export const gen2 = {
+  pembina: [
+    { nama: 'Yulva Romhida', jabatan: 'Pembina', ttl: 'Madiun, 1 Januari 1980', sosmed: '@yulva_alfaiz', foto: LOGO },
+  ],
+  pelatih: [
+    { nama: 'Airlangga Sukmana Teja Bramata', jabatan: 'Pelatih', ttl: 'Madiun, 9 September 1999', sosmed: '@airlanggabrmt_', foto: 'https://img1.pixhost.to/images/11273/677040669_uploaded_image.jpg' },
+    { nama: "Enrico Bryan's", jabatan: 'Pelatih', ttl: 'Madiun, 9 November 2001', sosmed: '@enrico_bryans', foto: LOGO },
+  ],
+  inti: [
+    { nama: 'Rizky Indra Permana', jabatan: 'Ketua Umum', ttl: 'Madiun, 15 Maret 2007', sosmed: '@rizkyndra', foto: `${BASE}/indra.jpg` },
+    { nama: 'Dwi Bella Noviyanti', jabatan: 'Wakil Ketua Umum', ttl: 'Madiun, 20 April 2007', sosmed: '@dwibella', foto: `${BASE}/bella.jpg` },
+    { nama: 'Anastasya Putri Giantio', jabatan: 'Sekretaris', ttl: 'Madiun, 10 Mei 2007', sosmed: '@anastasya', foto: `${BASE}/anas.jpg` },
+    { nama: 'Khofifah Tabina Azka Listaningtyas', jabatan: 'Bendahara', ttl: 'Madiun, 5 Juni 2007', sosmed: '@khofifah', foto: `${BASE}/pipah.jpg` },
+    { nama: 'Satria Nur Hidayatullah', jabatan: 'DTP', ttl: 'Madiun, 12 Juli 2007', sosmed: '@satria_nh', foto: `${BASE}/satria.jpg` },
+    { nama: 'Reyna Natasya Wahyu Safitri', jabatan: 'DTP', ttl: 'Madiun, 18 Agustus 2007', sosmed: '@reyna_natasya', foto: `${BASE}/reyna.jpg` },
+  ],
+  divisi: [
+    { nama: 'Ramzy Cahya Fauzan', jabatan: 'Koor GK3', ttl: 'Madiun, 22 September 2007', sosmed: '@ramzy_cf', foto: `${BASE}/ramzy.jpg` },
+    { nama: 'Sapta Andika Riyanto', jabatan: 'GK3', ttl: 'Madiun, 30 Oktober 2007', sosmed: '@sapta_ar', foto: `${BASE}/sapta.jpg` },
+    { nama: 'Farhan Rizky Devannanda', jabatan: 'GK3', ttl: 'Madiun, 5 November 2007', sosmed: '@farhan_rd', foto: `${BASE}/farhan.jpg` },
+    { nama: 'Keisya Nabila', jabatan: 'GK3', ttl: 'Madiun, 14 Desember 2007', sosmed: '@keisya', foto: `${BASE}/keisya.jpg` },
+    { nama: 'Tata Regita', jabatan: 'Koor Disarda', ttl: 'Madiun, 8 Januari 2008', sosmed: '@tata_regita', foto: `${BASE}/tata.jpg` },
+    { nama: 'Amanda Regina Putri Yuditira', jabatan: 'Disarda', ttl: 'Madiun, 20 Februari 2008', sosmed: '@disarda1', foto: `${BASE}/amanda.jpg` },
+    { nama: 'Ailen Listiani', jabatan: 'Disarda', ttl: 'Madiun, 20 Februari 2008', sosmed: '@disarda1', foto: `${BASE}/ailen.jpg` },
+    { nama: 'Lael Rahmadhani', jabatan: 'Disarda', ttl: 'Madiun, 20 Februari 2008', sosmed: '@disarda1', foto: `${BASE}/lael.jpg` },
+    { nama: 'Dea Alif Purwandini', jabatan: 'Disarda', ttl: 'Madiun, 20 Februari 2008', sosmed: '@disarda1', foto: `${BASE}/dea.jpg` },
+    { nama: 'Rizky Dava Mahendra', jabatan: 'Disarda', ttl: 'Madiun, 20 Februari 2008', sosmed: '@disarda1', foto: `${BASE}/dava.jpg` },
+    { nama: 'Alan Pratama', jabatan: 'Disarda', ttl: 'Madiun, 20 Februari 2008', sosmed: '@disarda1', foto: `${BASE}/alan.jpg` },
+    { nama: 'Yohanes David Adhienarta Putra', jabatan: 'Disarda', ttl: 'Madiun, 20 Februari 2008', sosmed: '@disarda1', foto: `${BASE}/david.jpg` },
+    { nama: 'Aditya Dwi Pratama', jabatan: 'Disarda', ttl: 'Madiun, 20 Februari 2008', sosmed: '@disarda1', foto: `${BASE}/adit.jpg` },
+    { nama: 'Alfarel Belva Falarossy', jabatan: 'Disarda', ttl: 'Madiun, 20 Februari 2008', sosmed: '@disarda1', foto: `${BASE}/farel.jpg` },
+    { nama: 'Giovanni Adhi Pratama', jabatan: 'Koor Infokom', ttl: 'Madiun, 24 Maret 2008', sosmed: '@whoisvex', foto: `${BASE}/vex.png` },
+    { nama: 'Desy Ariana Putri', jabatan: 'Infokom', ttl: 'Madiun, 20 Februari 2008', sosmed: '@desy', foto: `${BASE}/desy.jpg` },
+    { nama: 'Yusilla Anggun Vauli', jabatan: 'Infokom', ttl: 'Madiun, 20 Februari 2008', sosmed: '@disarda1', foto: `${BASE}/yusilla.jpg` },
+    { nama: 'Alisya Desvita Putri', jabatan: 'Infokom', ttl: 'Madiun, 20 Februari 2008', sosmed: '@disarda1', foto: `${BASE}/alisya.jpg` },
+  ],
+}
+
+// ─── GENERASI 3 ───────────────────────────────────────────────────────────────
+
+export const gen3 = {
+  pembina: [
+    { nama: 'Yulva Romhida', jabatan: 'Pembina', ttl: 'Madiun, 1 Januari 1980', sosmed: '@yulva_alfaiz', foto: LOGO },
+  ],
+  pelatih: [
+    { nama: 'Airlangga Sukmana Teja Bramata', jabatan: 'Pelatih', ttl: 'Madiun, 9 September 1999', sosmed: '@airlanggabrmt_', foto: 'https://img1.pixhost.to/images/11273/677040669_uploaded_image.jpg' },
+    { nama: "Enrico Bryan's", jabatan: 'Pelatih', ttl: 'Madiun, 9 November 2001', sosmed: '@enrico_bryans', foto: LOGO },
+  ],
+  inti: [
+    { nama: 'Muhammad Afrizal Nurjananta', jabatan: 'Ketua Umum', ttl: 'Madiun, 9 Maret 2009', sosmed: '@Afri.jaaal', foto: `${BASE}/afrizal.jpg` },
+    { nama: 'Fatimah Az-Zahra', jabatan: 'Wakil Ketua Umum', ttl: 'Madiun, 26 Juli 2008', sosmed: '@aarzhhh_', foto: `${BASE}/zahra.jpg` },
+    { nama: 'Nazuwa Qurroti Aqyuni Hasna', jabatan: 'Sekretaris 1', ttl: 'Magetan, 28 Mei 2009', sosmed: '@nanasywaww', foto: `${BASE}/nazuwa.jpg` },
+    { nama: 'Asyifa Najwa Nabilla Putri', jabatan: 'Sekretaris 2', ttl: 'Kota Madiun, 14 Januari 2009', sosmed: '@syi1.faaa_', foto: `${BASE}/asyifa.jpg` },
+    { nama: 'Eva Devi Setyorini', jabatan: 'Bendahara 1', ttl: 'Madiun, 8 April 2008', sosmed: '@evadvsty', foto: `${BASE}/eva.jpg` },
+    { nama: 'Winni Lestari Arti Ningsih', jabatan: 'Bendahara 2', ttl: 'Madiun, 6 Januari 2009', sosmed: '@Lovly_winn', foto: `${BASE}/IMG-20260213-WA0008.jpg` },
+    { nama: 'Yoga Prasetyo Wibowo', jabatan: 'DTP', ttl: 'Madiun, 27 Oktober 2008', sosmed: '@yogaprsttyo', foto: `${BASE}/yoga.jpg` },
+    { nama: 'Lelly Anggraini', jabatan: 'DTP', ttl: 'Sarolangun, 15 Januari 2009', sosmed: '@erjoyedlia', foto: `${BASE}/lelly.webp` },
+  ],
+  divisi: [
+    { nama: 'Fahri Lintang Saputra', jabatan: 'Koor GK3', ttl: 'Madiun, 11 Februari 2009', sosmed: '@fls._.110209', foto: `${BASE}/nopal.JPG` },
+    { nama: 'Clarinta Jida Valery', jabatan: 'GK3', ttl: 'Pekanbaru, 31 Mei 2009', sosmed: '@yinsatoru', foto: `${BASE}/clarinta.jpg` },
+    { nama: 'Nur Azizah Lailatul Munawaroh', jabatan: 'GK3', ttl: 'Magetan, 19 Maret 2008', sosmed: '@azizuumll', foto: `${BASE}/nur.jpg` },
+    { nama: 'Violita Ayu Wardani', jabatan: 'GK3', ttl: 'Madiun, 29 Mei 2009', sosmed: '@Piosyalalalaa', foto: `${BASE}/violita.jpg` },
+    { nama: 'Aymel Oktavia Fitroh Masyaroh', jabatan: 'GK3', ttl: 'Magetan, 25 Oktober 2008', sosmed: '@_Xyviii08', foto: `${BASE}/aymel.jpg` },
+    { nama: 'Suci Rofiqoh', jabatan: 'Koor Disarda', ttl: 'Kota Madiun, 30 Desember 2008', sosmed: '@_niicth', foto: `${BASE}/suci.jpg` },
+    { nama: 'Nabila Kayyisa Putri', jabatan: 'Disarda', ttl: 'Madiun, 5 April 2009', sosmed: '@nbilakyyisa', foto: `${BASE}/nabila.jpg` },
+    { nama: 'Rolly Sadion Astyani', jabatan: 'Disarda', ttl: 'Magetan, 30 Desember 2008', sosmed: '@rolisa__30', foto: `${BASE}/rolly.jpeg` },
+    { nama: 'Subhanifa Dwi Verbyanti', jabatan: 'Disarda', ttl: 'Madiun, 17 Februari 2009', sosmed: '@nadivrya', foto: `${BASE}/subhanifah.jpg` },
+    { nama: 'Mariska Cahya Dwi Maulida', jabatan: 'Disarda', ttl: 'Magetan, 20 Maret 2009', sosmed: '@maris.kachyaa', foto: `${BASE}/mariska.jpg` },
+    { nama: "Jahra' Anasula Farianto", jabatan: 'Disarda', ttl: 'Madiun, 10 November 2008', sosmed: '@meliorism.ysea', foto: `${BASE}/yasmin.jpg` },
+    { nama: 'Ella Rahmadani Elliana', jabatan: 'Disarda', ttl: 'Madiun, 20 Februari 2008', sosmed: '@ellll', foto: `${BASE}/ella.png` },
+    { nama: 'Silvia Nur Ramadhani', jabatan: 'Disarda', ttl: 'Madiun, 28 Agustus 2008', sosmed: '@slvvr4m4', foto: `${BASE}/silvi.jpg` },
+    { nama: 'Adly Surya Prasetiyo', jabatan: 'Disarda', ttl: 'Kota Madiun, 17 November 2008', sosmed: '@718dly', foto: `${BASE}/adly.jpg` },
+    { nama: 'Meysa Desta Maharani', jabatan: 'Disarda', ttl: 'Kota Madiun, 12 Mei 2008', sosmed: '@mey.ajlh_', foto: `${BASE}/meysa.webp` },
+    { nama: 'Richy Yuga Tri Finalya', jabatan: 'Koor Infokom', ttl: 'Madiun, 29 Desember 2007', sosmed: '@rchy.yg', foto: `${BASE}/Richy%20Yuga.webp` },
+    { nama: 'Anisa Apriliana Tunggal Dewi', jabatan: 'Infokom', ttl: 'Madiun, 12 April 2008', sosmed: '@o_oveuc', foto: `${BASE}/anisa.jpg` },
+    { nama: 'Bhilqis Tian Zahrotushita', jabatan: 'Infokom', ttl: 'Bogor, 28 April 2009', sosmed: '@bluetz_summer', foto: `${BASE}/bilqis.jpg` },
+    { nama: 'Az Zahrah Khanza Dewi Kuwais', jabatan: 'Infokom', ttl: 'Madiun, 3 Oktober 2008', sosmed: '@bibbleeze', foto: `${BASE}/khanza.jpg` },
+    { nama: 'Nindia Artita Lestari', jabatan: 'Infokom', ttl: 'Madiun, 13 Februari 2009', sosmed: '@ndiaayyl', foto: `${BASE}/nindia.jpg` },
+    { nama: 'Fahrel Akbar', jabatan: 'Infokom', ttl: 'Madiun, 1 Oktober 2008', sosmed: '@f4ahrell', foto: `${BASE}/fahrel.jpg` },
+    { nama: 'Janathan Hafiz', jabatan: 'Infokom', ttl: 'Madiun, 14 Januari 2009', sosmed: '@janatan_official_', foto: `${BASE}/nathan.jpg` },
+  ],
+}
